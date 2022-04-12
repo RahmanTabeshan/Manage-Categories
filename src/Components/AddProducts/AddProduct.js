eb3943a5f543c4855ee3ce936c474c8754cf8545
Rahman Tabeshan <rtabeshan33@gmail.com>
2022-04-12 18:18:42 +0430

    add selectBox of material ui

--- src/Components/AddProducts/AddProduct.js
+++ src/Components/AddProducts/AddProduct.js
@@ -1,6 +1,10 @@
 
 import Input from "../Input/Input";
 import "./AddProduct.css" ;
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
 
 const AddProduct = ({form , categories , onClick , onChangeInput , onChangeSelect }) => {
     
@@ -15,14 +19,29 @@ const AddProduct = ({form , categories , onClick , onChangeInput , onChangeSelec
                 el="product"
             />
             <div className="category-data">
-                <select className="category-select" value={form.select} onChange={(e)=>onChangeSelect(e)}>
-                    <option value={0} > انتخاب دسته </option>
+                {/* <select className="category-select" value={form.select} onChange={(e)=>onChangeSelect(e)}>
+                    {form.select ? null : <option value={0} > انتخاب دسته </option>}
                     {categories.map(item =>{
                         return(
                             <option key={item.id} value={item.id}>{item.name}</option>
                         )
                     })}
-                </select>
+                </select> */}
+                 <FormControl className="form-select" >
+                    <InputLabel id="select-label">دسته ها</InputLabel>
+                    <Select
+                        labelId="select-label"
+                        className="select"
+                        value={form.select}
+                        label="دسته ها"
+                        onChange={(e)=>onChangeSelect(e)}
+                    >
+                        {form.select ? null : <MenuItem value={0}>انتخاب دسته</MenuItem>}
+                        {categories.map(item=>{
+                            return <MenuItem key={item.id} value={item.id} >{item.name}</MenuItem> ;
+                        })}
+                    </Select>
+                </FormControl>
             </div>
         </section>
     );
